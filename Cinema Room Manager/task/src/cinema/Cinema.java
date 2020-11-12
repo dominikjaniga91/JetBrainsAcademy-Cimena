5db9df980f893c2ef58887146f71be3423923399
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-11-12 19:43:57 +0100

    Calculate price for entire cinema in stage two

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,29 +1,46 @@
 package cinema;
 
-import java.io.PrintStream;
-import java.util.stream.IntStream;
+import java.util.Scanner;
 
 public class Cinema {
-    private final static PrintStream out = System.out;
-    private final static int NUMBER_OF_ROWS = 7;
-    private final static int NUMBER_OF_SEATS = 8;
 
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        printColumnsNumbers();
-        printSeats();
+
+        Scanner scanner = new Scanner(System.in);
+        Cinema cinema = new Cinema();
+        int frontSeatPrice = 10;
+        int backSeatPrice = 8;
+
+        int rows = cinema.getNumberOfRows(scanner);
+        int seatsInRow = cinema.getNumberOfSeats(scanner);
+        int frontRows = getFrontRows(rows);
+        int backRows = getBackRows(rows, frontRows);
+        int frontSeatsCost = cinema.calculatePrice(frontRows, seatsInRow, frontSeatPrice);
+        int backSeatsCost = cinema.calculatePrice(backRows, seatsInRow, backSeatPrice);
+        int totalCost = frontSeatsCost + backSeatsCost;
+
+        System.out.printf("Total income: %n$%s", totalCost);
     }
 
-    private static void printColumnsNumbers() {
-        out.print("  ");
-        IntStream.rangeClosed(1, NUMBER_OF_SEATS)
-                .forEach(i -> out.print(i + " "));
-        out.println();
+    private static int getBackRows(int rows, int frontRows) {
+        return rows > 4 ? rows - frontRows : 0;
     }
 
-    private static void printSeats() {
-        String seats = "S ".repeat(NUMBER_OF_SEATS);
-        IntStream.rangeClosed(1, NUMBER_OF_ROWS)
-                .forEach(number -> out.printf("%d %s%n",number, seats));
+    private static int getFrontRows(int rows) {
+        return rows > 4 ? rows / 2 : rows;
+    }
+
+    private int getNumberOfRows(Scanner scanner) {
+        System.out.println("Enter the number of rows:");
+        return scanner.nextInt();
+    }
+
+    private int getNumberOfSeats(Scanner scanner) {
+        System.out.println("Enter the number of seats in each row:");
+        return scanner.nextInt();
+    }
+
+    private int calculatePrice(int rows, int seats, int price) {
+        return rows * seats * price;
     }
 }
