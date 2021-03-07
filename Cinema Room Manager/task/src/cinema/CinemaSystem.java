1bba6e2c414a01677096d1ca91483b5c6c5eff2a
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-03-07 15:50:54 +0100

    Create CinemaSystem

---
+++ Cinema Room Manager/task/src/cinema/CinemaSystem.java
@@ -0,0 +1,43 @@
+package cinema;
+
+import java.util.Scanner;
+
+class CinemaSystem {
+
+    private final CinemaManager cinemaManager = new CinemaManager();
+    private final Scanner scanner = new Scanner(System.in);
+    private Cinema cinema;
+
+    void start() {
+        int rows = getNumberOfRows(scanner);
+        int seatsInRow = getNumberOfSeats(scanner);
+        cinema = cinemaManager.createCinema(rows, seatsInRow);
+        cinema.print();
+
+
+        Seat userSeat = readUserPlace(scanner);
+        int price = cinema.calculatePrize(userSeat);
+        System.out.printf("%nTicket price: $%d%n", price);
+        cinema.bookSeat(userSeat);
+        cinema.print();
+
+    }
+
+    private static Seat readUserPlace(Scanner scanner) {
+        System.out.println("Enter a row number:");
+        int row = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int column = scanner.nextInt();
+        return new Seat(row, column);
+    }
+
+    private static int getNumberOfRows(Scanner scanner) {
+        System.out.println("Enter the number of rows:");
+        return scanner.nextInt();
+    }
+
+    private static int getNumberOfSeats(Scanner scanner) {
+        System.out.println("Enter the number of seats in each row:");
+        return scanner.nextInt();
+    }
+}
